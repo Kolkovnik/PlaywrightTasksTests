b7edc87fda10523888e077532ed07478de7059fc
Kolkovnik <kolkovnik111@mail.ru>
2026-03-19 19:07:28 +0300

    Добавлена и интегрирована автоматическая запись Playwright Traces с Allure. Создан README.md

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,11 @@
 import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class BaseTest {
@@ -16,11 +22,28 @@ public class BaseTest {
         );
         context = browser.newContext();
         page = context.newPage();
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
     }
 
     @AfterEach
-    void tearDown() {
-        if (context != null) context.close();
+    void tearDown(TestInfo testInfo) {
+        if (context != null) {
+            Path tracePath = Paths.get("build/traces/trace-" + testInfo.getDisplayName() + ".zip");
+            context.tracing().stop(new Tracing.StopOptions().setPath(tracePath));
+            try {
+                Allure.addAttachment("Playwright Trace", "application/zip",
+                        Files.newInputStream(tracePath), ".zip");
+            } catch (IOException e) {
+                System.err.println("Трассировка не подцепилась к Allure: " + e.getMessage());
+            }
+
+//            context.tracing().stop(new Tracing.StopOptions()
+//                    .setPath(Paths.get("build/traces/trace-" + testInfo.getDisplayName() + ".zip")));
+            context.close();
+        }
         if (browser != null) browser.close();
         if (playwright != null) playwright.close();
     }
